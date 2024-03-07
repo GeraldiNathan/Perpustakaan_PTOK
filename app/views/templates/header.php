839ab2f2739b40f0127874e391228df3cdd4c9f1
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-03-07 23:04:17 +0800

    Fix(config) : change port to 8000 in branch loginPage

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="http://localhost:8080/Perpustakaan_PTOK/app/output.css">
+    <link rel="stylesheet" href="http://localhost:8000/Perpustakaan_PTOK/app/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
