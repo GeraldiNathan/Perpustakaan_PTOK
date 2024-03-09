319f28d51a3307705b02f71ab5517a1baefe23a0
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-03-10 03:25:48 +0800

    fix(Config) : commented localhost with port

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,8 @@
 <?php
-define('BASEURL', 'http://localhost:8000/Perpustakaan_PTOK/public');
-define('NODEURL', 'http://localhost:8000/Perpustakaan_PTOK/node_modules');
+// define('BASEURL', 'http://localhost:8080/Perpustakaan_PTOK/public');
+// define('NODEURL', 'http://localhost:8080/Perpustakaan_PTOK/node_modules');
+define('BASEURL', 'http://localhost/Perpustakaan_PTOK/public');
+define('NODEURL', 'http://localhost/Perpustakaan_PTOK/node_modules');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
